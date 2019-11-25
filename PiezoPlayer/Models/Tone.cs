10438b303714a9c6bc889839cd515d1f0a2921c6
Anders busk <anbusk92@gmail.com>
2019-11-25 20:56:11 +0100

    Song og tone

---
+++ PiezoPlayer/Models/Tone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PiezoPlayer.Models
+{
+    public class Tone
+    {
+        public Song song;
+        public Tone nextTone;
+        public Tone previousTone;
+        public Tone firstTone;
+        public Tone lastTone;
+        public int speakerIdToPlayOn;
+        public int delay;
+    }
+}
