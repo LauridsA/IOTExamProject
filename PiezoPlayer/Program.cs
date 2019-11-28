9cbfc60bad9f61d125b2d5c1efa8396830969525
Anders Busk <anders.busk.92@gmail.com>
2019-11-28 17:20:35 +0100

    Play

--- PiezoPlayer/Program.cs
+++ PiezoPlayer/Program.cs
@@ -21,7 +21,7 @@ namespace PiezoPlayer
         {
             PiezoPlayerClient pp = new PiezoPlayerClient();
             pp.SetupPlayer().Wait();
-            pp.PlayLocal().Wait();
+            pp.Play().Wait();
         }
     }
 }
