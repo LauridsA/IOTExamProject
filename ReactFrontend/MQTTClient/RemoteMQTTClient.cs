c192e9b38e75f95662d068c66c3deafc49d6ecb4
Anders Busk <anders.busk.92@gmail.com>
2019-12-08 15:16:26 +0100

    KOM SÅP

--- ReactFrontend/MQTTClient/RemoteMQTTClient.cs
+++ ReactFrontend/MQTTClient/RemoteMQTTClient.cs
@@ -28,7 +28,7 @@ namespace ReactFrontend.MQTTClient
             };
             string topicSubscribe = "song/#";
             string topicSubscribe2 = "status";
-            remoteClient = await MqttClient.CreateAsync("localhost", configuration);
+            remoteClient = await MqttClient.CreateAsync("broker.busk.cf", configuration);
             await remoteClient.ConnectAsync(new MqttClientCredentials(clientId: "Frontend", "front", "frontpass"));
             await remoteClient.SubscribeAsync(topicSubscribe, MqttQualityOfService.AtLeastOnce); //subscribe on cloud mqtt
             await remoteClient.SubscribeAsync(topicSubscribe2, MqttQualityOfService.AtLeastOnce); //subscribe on cloud mqtt
