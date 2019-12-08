1119f1f922b062db89b1e5676232249a57de96ab
Laurids Andersen <Laurids.Andersen@jypo.rootdom.dk>
2019-12-08 11:41:33 +0100

    added api endpoints, in memory db, and services

---
+++ ReactFrontend/DataLayer/AlbumDB.cs
@@ -0,0 +1,124 @@
+﻿using ReactFrontend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactFrontend.DataLayer
+{
+    public static class AlbumDB
+    {
+        public static Dictionary<int, Song> songs = new Dictionary<int, Song>()
+        {
+            {
+                1, new Song()
+                {
+                    title = "The Imperial March",
+                    nodes = new List<Tone>()
+                    {
+                        //speaker, frequency, duration(ms)
+                        new Tone(1, 5, 500),
+                        new Tone(1, 5, 500),
+                        new Tone(1, 5, 500),
+                        new Tone(1, 5, 350),
+                        new Tone(1, 5, 150),
+                        new Tone(1, 5, 500),
+                        new Tone(1, 5, 350),
+                        new Tone(1, 5, 150),
+                        new Tone(1, 5, 650),
+                        //small wait, no tone
+                        new Tone(1, 0, 150),
+                        //next block
+                        new Tone(1, 5, 500),
+                        new Tone(1, 5, 500),
+                        new Tone(1, 5, 500),
+                        new Tone(1, 5, 350),
+                        new Tone(1, 5, 150),
+                        new Tone(1, 5, 350),
+                        //next small wait, no tone
+                        new Tone(1, 0, 250),
+                        //next block
+                        new Tone(1, 5, 500),
+                        new Tone(1, 5, 300),
+                        new Tone(1, 5, 150),
+                        new Tone(1, 5, 400),
+                        new Tone(1, 5, 200),
+                        new Tone(1, 5, 200),
+                        new Tone(1, 5, 125),
+                        new Tone(1, 5, 125),
+                        new Tone(1, 5, 250),
+                        //next small wait, no tone
+                        new Tone(1, 0, 250),
+                        //next block
+                        new Tone(1, 5, 250),
+                        new Tone(1, 5, 400),
+                        new Tone(1, 5, 200),
+                        new Tone(1, 5, 200),
+                        new Tone(1, 5, 125),
+                        new Tone(1, 5, 125),
+                        new Tone(1, 5, 250),
+                        //next small wait, no tone
+                        new Tone(1, 0, 250),
+                        //last block
+                        new Tone(1, 5, 125),
+                        new Tone(1, 5, 500),
+                        new Tone(1, 5, 375),
+                        new Tone(1, 5, 125),
+                        new Tone(1, 5, 500),
+                        new Tone(1, 5, 375),
+                        new Tone(1, 5, 125),
+                        new Tone(1, 5, 650)
+                    }
+                }
+            },
+            {
+                2, new Song()
+                {
+                    title = "My Other Song",
+                    nodes = new List<Tone>()
+                    {
+                        //speaker, frequency, duration, delaybeforeplaying
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1)
+                    }
+                }
+            },
+            {
+                3, new Song()
+                {
+                    title = "Go Suck A D",
+                    nodes = new List<Tone>()
+                    {
+                        //speaker, frequency, duration, delaybeforeplaying
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1),
+                        new Tone(1, 5, 1)
+                    }
+                }
+            },
+        };
+
+        public static Dictionary<int, Song> GetSongs()
+        {
+            return songs;
+        }
+
+    }
+}
---
+++ ReactFrontend/Services/AlbumService.cs
@@ -0,0 +1,34 @@
+﻿using ReactFrontend.DataLayer;
+using ReactFrontend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReactFrontend.Services
+{
+    public class AlbumService
+    {
+        public AlbumService()
+        {
+            //empty lul
+        }
+
+        public Song getSong(int key)
+        {
+            var songs = AlbumDB.GetSongs();
+            if (key+1 > songs.Count)
+            {
+                return songs[1];
+            }
+            return songs[key];
+        }
+
+        public Song getSongFromTitle(string title)
+        {
+            var songs = AlbumDB.GetSongs();
+            var song = songs.Values.FirstOrDefault((list) => list.title == title);
+            return song;
+        }
+    }
+}
