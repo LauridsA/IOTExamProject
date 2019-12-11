4a3c3981a103487353e79c8b1c2ab8a823b6f4e7
Anders Busk <anders.busk.92@gmail.com>
2019-12-11 16:09:17 +0100

    lets try dis shits

---
+++ Player2/Program.cs
@@ -0,0 +1,21 @@
+﻿using NAudio.Wave;
+using System;
+
+namespace Player2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var waveOut = new WaveOutEvent();
+            var mp3Reader = new Mp3FileReader(@"untitled.mp3");
+            //BufferedWaveProvider buffer = new BufferedWaveProvider(WaveFormat.)
+            waveOut.Init(mp3Reader);
+            waveOut.Play();
+
+            Console.Read();
+        }
+    }
+}
